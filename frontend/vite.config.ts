bb7075869e175d7d4ef9862eccaa55b48a7ca335
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-04-12 10:47:10 +0100

    fix: updated vite config host so app can be accessed when running from dev container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0'  // Bind to all interfaces so app can be accessed when running in devcontainer
+  }
 })
