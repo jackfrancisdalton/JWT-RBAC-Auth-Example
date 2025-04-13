9f82af5ed2bc6c1af0c16251cfbba995d279bf22
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-04-13 14:20:31 +0000

    chore: added todo around port setting and CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173', // TODO: update the dev and prod versions to always point to the same port (80), and have value set via env variable in docker-compose
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
