f00b201a5aef42d677c08c7341be9e6022f5cde5
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-04-13 14:19:43 +0000

    feat: added place holders for authentication state

---
+++ frontend/src/auth/components/AuthenticationStateCard.tsx
@@ -0,0 +1,8 @@
+export default function AuthenticationStateCard() {
+    return (
+        <div>
+            {/* TODO content */}
+            <p>Display the current user, roles, etc... </p>
+        </div>
+    )
+}
---
+++ frontend/src/auth/pages/AuthenticatedPage.tsx
@@ -0,0 +1,10 @@
+import AuthenticationStateCard from "../components/AuthenticationStateCard";
+
+export default function AuthenticatedPage() {
+    return (
+        <div>
+            {/* TODO content */}
+            <AuthenticationStateCard></AuthenticationStateCard>
+        </div>
+    )
+}
