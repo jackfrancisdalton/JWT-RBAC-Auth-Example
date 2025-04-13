8826cd1f7cced722abdf481445458d0ad7c1ef21
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-04-13 22:49:04 +0000

    chore: clean up protected routes behaviour

--- frontend/src/auth/components/ProtectedRoute.tsx
+++ frontend/src/auth/components/ProtectedRoute.tsx
@@ -1,23 +1,31 @@
 import { PropsWithChildren } from 'react';
 import { Role, useAuth } from '../providers/AuthProvider';
 import ThisIsBlockedPage from '../pages/ThisIsBlockedPage';
+import { Navigate } from 'react-router-dom';
 
 type ProtectedRouteProps =  {
     requiredRoles: Role[]; 
 } & PropsWithChildren;
 
 const ProtectedRoute = ({ children, requiredRoles }: ProtectedRouteProps) => {
-  const { user } = useAuth();
+    const { user } = useAuth();
 
     // User starts as undefined so we treat this as a loading state
     if (user === undefined) {
         return <div>Loading...</div> // TODO: replace with a loading component
     }
 
-    if (!user || !requiredRoles.every(role => user.roles.includes(role))) {
+    // If user is not logged in redirect to login page
+    if (!user) {
+        return <Navigate to="/login" replace />;
+    }
+
+    // If user is logged in but lacks permissions show lock page
+    if (!requiredRoles.every(role => user.roles.includes(role))) {
         return <ThisIsBlockedPage requiredRoles={requiredRoles}></ThisIsBlockedPage>
     }
 
+    // else render the children
     return children;
 };
 
